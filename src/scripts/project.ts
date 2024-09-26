3bac4cef635f40933576c5860c1d0435cce8ea24
AkshatGKMit <akshat@gkmit.co>
2024-09-26 23:14:30 +0530

    feat: Add count completed project function

--- src/scripts/project.ts
+++ src/scripts/project.ts
@@ -1,3 +1,8 @@
+export function countCompletedProjects(developer: Developer): number {
+	const count = developer.projects.reduce((total, { isCompleted }) => (total += isCompleted ? 1 : 0), 0);
+	return count;
+}
+
 export function listProjects(developer: Developer): string[] {
 	const projectNames: string[] = developer.projects.map((project) => project.name);
 	return projectNames;
