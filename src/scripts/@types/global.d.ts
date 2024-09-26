2ee3d70f36d5cd597ea14e8df7fbb2c2d3996552
AkshatGKMit <akshat@gkmit.co>
2024-09-26 21:10:03 +0530

    feat: Add interface in global declaration file

---
+++ src/scripts/@types/global.d.ts
@@ -0,0 +1,16 @@
+declare interface Project {
+	readonly id: string;
+	name: string;
+	techStack: string[];
+	isCompleted: boolean;
+}
+
+declare interface Developer {
+	readonly id: string;
+	name: string;
+	age: number;
+	isEmployed: boolean;
+	skills: string[];
+	projects: Project[];
+	experience: number;
+}
