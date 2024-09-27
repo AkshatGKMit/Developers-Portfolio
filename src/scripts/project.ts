389a9e6e127624179e4ae410b36aa81e802cd7c0
AkshatGKMit <akshat@gkmit.co>
2024-09-27 15:05:36 +0530

    fix: Minor fixes

--- src/scripts/project.ts
+++ src/scripts/project.ts
@@ -1,27 +1,22 @@
 import { generateId } from "./helpers/index.js";
 
-export function addProject(developer: Developer, project: Partial<Project>) {
+export function addProject({ projects }: Developer, project: Partial<Project>) {
 	const isProjectValid = validateProject(project);
 	if (!isProjectValid) return;
 
-	const validatedProject: Project = { ...project, id: generateId(developer.projects) } as Developer;
-	developer.projects.push(validatedProject);
+	const validatedProject = { ...project, id: generateId(projects) };
+	projects.push(validatedProject as Project);
 }
 
 export function validateProject(project: Partial<Project>): boolean {
 	const { name, techStack } = project;
-
-	if (!name) {
-		alert("Project name is required");
-		return false;
-	}
 
 	if (!techStack || techStack.length < 2) {
 		alert("At least two technologies are required for a project");
 		return false;
 	}
 
-	if (!techStack.some((tech) => tech.toLowerCase().includes("git"))) {
+	if (!techStack.some((tech: string) => tech.toLowerCase().includes("git"))) {
 		alert("Tech stack must contain git");
 		return false;
 	}
@@ -29,12 +24,12 @@ export function validateProject(project: Partial<Project>): boolean {
 	return true;
 }
 
-export function countCompletedProjects(developer: Developer): number {
-	const count = developer.projects.reduce((total, { isCompleted }) => (total += isCompleted ? 1 : 0), 0);
+export function countCompletedProjects({ projects }: Developer): number {
+	const count = projects.reduce((total, { isCompleted }) => (total += isCompleted ? 1 : 0), 0);
 	return count;
 }
 
-export function listProjects(developer: Developer): string[] {
-	const projectNames: string[] = developer.projects.map((project) => project.name);
+export function listProjects({ projects }: Developer): string[] {
+	const projectNames: string[] = projects.map(({ name }: Project) => name);
 	return projectNames;
 }
